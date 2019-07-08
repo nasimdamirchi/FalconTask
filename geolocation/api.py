6ba1151699d996784219f620cbfd4ccceecc8152
Nasim <Nasim.damichli89@gmail.com>
2019-07-08 16:59:42 +0200

    Add Async
    Added endpoints:
     - POST geocode : create celery task, return task id
     - Post reverse : create celery task, return task id
     - Get  geo_result: get state and result of created task
    
     Geocode and Reverse geo code will be processed asynchronously using celery

--- geolocation/api.py
+++ geolocation/api.py
@@ -3,56 +3,37 @@ import http
 
 from flask import Flask, request
 
-from geopy.geocoders import Nominatim
+from celery_config import geocode, reverse_geocode, celery
 
 app = Flask(__name__)
 
 
-def geocode(address):
-    geo_locator = Nominatim(user_agent='nasim.damirchli89@gmail.com')
-    locations = geo_locator.geocode(address, exactly_one=False)
-    result = []
-    if locations:
-        for location in locations:
-            result.append({"address": location.address, "lat": location.latitude, "long": location.longitude})
-    return result
-
-
-def reverse_geocode(lat, lon):
-    geo_locator = Nominatim(user_agent='nasim.damirchli89@gmail.com')
-    locations = geo_locator.reverse((str(lat), str(lon)), exactly_one=False)
-    result = []
-    if locations:
-        for location in locations:
-            result.append({"address": location.address, "lat": location.latitude, "long": location.longitude})
-    return result
-
-
-@app.route('/geocode', methods=['GET'])
-def get_geocode():
-    address = request.args.get('address', '')
+@app.route('/geocode', methods=['POST'])
+def start_geocode():
+    address = request.get_json(force=True).get('address')
     try:
-        response = geocode(address)
-        if response:
-            return json.dumps(response, ensure_ascii=False)
-        else:
-            return '', http.HTTPStatus.NO_CONTENT
-    except Exception:
-        return 'Some error occurred ...', http.HTTPStatus.INTERNAL_SERVER_ERROR
+        result = geocode.delay(address)
+        return result.id, http.HTTPStatus.ACCEPTED
+    except Exception as e:
+        return 'Some error occurred ... {}'.format(e), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@app.route('/reverse', methods=['GET'])
-def get_reverse_geocode():
-    lat = request.args.get('lat', '')
-    lon = request.args.get('lon', '')
+@app.route('/reverse', methods=['POST'])
+def start_reverse_geocode():
+    lat = request.get_json(force=True).get('lat')
+    lon = request.get_json(force=True).get('lon')
     try:
-        response = reverse_geocode(lat, lon)
-        if response:
-            return json.dumps(response, ensure_ascii=False)
-        else:
-            return '', http.HTTPStatus.NO_CONTENT
-    except Exception:
-        return 'Some error occurred ...', http.HTTPStatus.INTERNAL_SERVER_ERROR
+        result = reverse_geocode.delay(lat, lon)
+        return result.id, http.HTTPStatus.ACCEPTED
+    except Exception as e:
+        return 'Some error occurred ... {}'.format(e), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@app.route('/geo_result/<job_id>', methods=['GET'])
+def geocode_result(job_id):
+    job = celery.AsyncResult(job_id)
+    response_obj = {'status': job.state, 'result': job.result}
+    return json.dumps(response_obj, ensure_ascii=False)
 
 
 if __name__ == '__main__':
---
+++ geolocation/celery_config.py
@@ -0,0 +1,28 @@
+from celery import Celery
+
+from geopy.geocoders import Nominatim
+
+celery = Celery('celery_config', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1',
+                include=['api', ], )
+
+
+@celery.task()
+def geocode(address):
+    geo_locator = Nominatim(user_agent='nasim.damirchli89@gmail.com')
+    locations = geo_locator.geocode(address, exactly_one=False)
+    result = []
+    if locations:
+        for location in locations:
+            result.append({"address": location.address, "lat": location.latitude, "long": location.longitude})
+    return result
+
+
+@celery.task()
+def reverse_geocode(lat, lon):
+    geo_locator = Nominatim(user_agent='nasim.damirchli89@gmail.com')
+    locations = geo_locator.reverse((str(lat), str(lon)), exactly_one=False)
+    result = []
+    if locations:
+        for location in locations:
+            result.append({"address": location.address, "lat": location.latitude, "long": location.longitude})
+    return result
